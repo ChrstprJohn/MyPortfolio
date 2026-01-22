c4a9deac5cad454713373650564733c9b6aad063
Christopher John <picardochristopherjohnoleo1@gmail.com>
2026-01-22 12:09:50 +0800

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,41 @@
+import { navIcons, navLinks } from '#constants';
+import React from 'react';
+import dayjs from 'dayjs';
+
+const Navbar = () => {
+    return (
+        <nav>
+            <div>
+                <img
+                    src='/images/logo.svg'
+                    alt='logo'
+                />
+                <p className='font-bold'>Picardo Portfolio</p>
+
+                <ul>
+                    {navLinks.map(({ id, name }) => (
+                        <li key={id}>{name}</li>
+                    ))}
+                </ul>
+            </div>
+
+            <div>
+                <ul>
+                    {navIcons.map(({ id, img }) => (
+                        <li key={id}>
+                            <img
+                                src={img}
+                                className='icon-hover'
+                                alt={`icon${id}`}
+                            />
+                        </li>
+                    ))}
+                </ul>
+
+                <time>{dayjs().format('ddd MMM D h:mm')}</time>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
